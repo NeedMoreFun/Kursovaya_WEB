5399dfb522babaccc1d743043bd4943c905c6b58
NeedMoreFun <70326266+NeedMoreFun@users.noreply.github.com>
2021-12-24 18:30:49 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js">
-function slowScroll(id) {
-     $('html, body').animate({scrollTop: $(id).offset().top}, 500);
- }
-document.getElementById('mess_send').addEventListener('click', function (){
-      alert('Сообщение отправлено!');
-      window.location.reload(true);
-});
